71d4bb7e431602a38d5a0f3ab3d1f95b46a9efbb
Maksym Chervinskyi <66177888+Gaivir@users.noreply.github.com>
2021-04-29 23:41:26 +0300

    Працює над дз 11-2

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-
-import './styles.css';
-
-
-const delay = ms => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(ms);
-        }, `${ms}`);
-    
-    })
-}
-
-
-const logger = time => console.log(`Resolved after ${time}ms`);
-
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
-
-
-console.log('Hello');
+import "./js/task-01";
+import "./js/task-02";
+import "./js/task-03";
